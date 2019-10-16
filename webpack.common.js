79af7f980bfa61e922517703588cbf464a572e15
Attapon <attapon@chameleonstrategies.com>
2019-10-16 23:16:54 +0700

    update webcomponents and webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,9 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = {
-  entry: "./src/index.js",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html"
-    })
-  ],
+  entry: {
+    main: "./src/index.js"
+  },
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader", 
-          "css-loader",
-          "sass-loader"
-        ]
-      },
       {
         test: /\.html$/,
         use: ["html-loader"]
@@ -28,7 +14,7 @@ module.exports = {
           loader: "file-loader",
           options: {
             name: "[name].[hash].[ext]",
-            outputPath: "assets"
+            outputPath: "assets/images"
           }
         }
       }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,44 @@
 const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
-    filename: "main.[contentHash].js",
+    filename: "[name].[contentHash].bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true
+      }
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin()
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin({filename:"[name].[contentHash].css"})],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, 
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+    ]
   }
 });
