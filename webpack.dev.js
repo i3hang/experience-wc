1602b92517368f583d134fb00af97a3b8f7198a1
Weerayut Teja <teja.weerayut@gmail.com>
2019-10-24 15:34:19 +0700

    Update sample on how to manipulate DOM Element inside Web Component

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
@@ -12,14 +13,17 @@ module.exports = merge(common, {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html"
-    })
+    }),
+    new CopyWebpackPlugin([
+      './src/data.json'
+    ])
   ],
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          "style-loader", 
+          "style-loader",
           "css-loader",
           "sass-loader"
         ]
